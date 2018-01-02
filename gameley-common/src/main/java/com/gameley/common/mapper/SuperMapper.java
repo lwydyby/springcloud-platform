834b43b403346e5ea7d8f4356a8d69fbcf949856
万万没想到 <qzywyd@163.com>
2018-01-02 19:21:52 +0800

    增加superMapper公共方法类

---
+++ gameley-common/src/main/java/com/gameley/common/mapper/SuperMapper.java
@@ -0,0 +1,13 @@
+package com.gameley.common.mapper;
+
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * mybatis-plus父类可以写自定义公共方法
+ * @param <T>
+ */
+public interface SuperMapper<T> extends BaseMapper<T> {
+
+
+}
