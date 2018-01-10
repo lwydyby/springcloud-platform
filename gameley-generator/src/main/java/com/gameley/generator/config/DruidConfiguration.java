f96a1a0114676b8e39bca8aec1c666ab42c15d1a
万万没想到 <qzywyd@163.com>
2018-01-10 14:52:45 +0800

    注释springcloud-config配置，以便本地单独运行

--- gameley-generator/src/main/java/com/gameley/generator/config/DruidConfiguration.java
+++ gameley-generator/src/main/java/com/gameley/generator/config/DruidConfiguration.java
@@ -1,47 +1,47 @@
-package com.gameley.generator.config;
-
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import com.alibaba.druid.pool.DruidDataSource;
-
-import lombok.extern.slf4j.Slf4j;
-
-/**
- *
- * 配置数据库
- * @author chhliu *
- * @version 1.2.0
- */
-@Slf4j
-@Configuration
-@RefreshScope
-public class DruidConfiguration {
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
-    @Value("${spring.datasource.driverClassName}")
-
-
-    @Bean     //声明其为Bean实例
-    @Primary  //在同样的DataSource中，首先使用被标注的DataSource
-    @RefreshScope
-    public DataSource dataSource(){
-        DruidDataSource datasource = new DruidDataSource();
-        datasource.setUrl(dbUrl);
-        datasource.setUsername(username);
-        datasource.setPassword(password);
-        return datasource;
-    }
-}
+//package com.gameley.generator.config;
+//
+//import java.sql.SQLException;
+//
+//import javax.sql.DataSource;
+//
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//import com.alibaba.druid.pool.DruidDataSource;
+//
+//import lombok.extern.slf4j.Slf4j;
+//
+///**
+// *
+// * 配置数据库
+// * @author wwmxd
+// * @version 1.2.0
+// */
+//@Slf4j
+//@Configuration
+//@RefreshScope
+//public class DruidConfiguration {
+//    @Value("${spring.datasource.url}")
+//    private String dbUrl;
+//    @Value("${spring.datasource.username}")
+//    private String username;
+//    @Value("${spring.datasource.password}")
+//    private String password;
+//    @Value("${spring.datasource.driverClassName}")
+//
+//
+//    @Bean     //声明其为Bean实例
+//    @Primary  //在同样的DataSource中，首先使用被标注的DataSource
+//    @RefreshScope
+//    public DataSource dataSource(){
+//        DruidDataSource datasource = new DruidDataSource();
+//        datasource.setUrl(dbUrl);
+//        datasource.setUsername(username);
+//        datasource.setPassword(password);
+//        return datasource;
+//    }
+//}
