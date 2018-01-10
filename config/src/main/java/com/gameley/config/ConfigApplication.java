4bd0d8a56e32c9c7b407084efb39d5d334e9d53c
万万没想到 <qzywyd@163.com>
2018-01-10 14:37:42 +0800

    增加springcloud-bus来完成动态数据库配置

--- config/src/main/java/com/gameley/config/ConfigApplication.java
+++ config/src/main/java/com/gameley/config/ConfigApplication.java
@@ -3,11 +3,11 @@ package com.gameley.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
+//@EnableEurekaClient
 public class ConfigApplication {
 
 	public static void main(String[] args) {
