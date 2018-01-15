f4f4612c2f481f8357ddbca9eb2b8b57a17cf1dd
万万没想到 <qzywyd@163.com>
2018-01-15 11:21:11 +0800

    完善日志记录

--- gameley-common/src/main/java/com/gameley/common/util/MapUtil.java
+++ gameley-common/src/main/java/com/gameley/common/util/MapUtil.java
@@ -6,7 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MapUtil{
-
+    /**
+     * 根据所传参数名称，将非空的参数值存入到map中返回
+     * @param jsonObject
+     * @param strings
+     * @return
+     */
     public static Map<String,Object> changeToMap(JSONObject jsonObject,String... strings){
         Map<String,Object> maps=new HashMap<>();
         for(String name:strings){
